70c028c2ebe6224242dda832a05668f8789b5e45
Mantissagithub <mantissa6789@gmail.com>
2026-02-27 18:37:38 +0530

    fix: update DPOTrainer configuration and dataset loading for improved training setup

--- dpo_trainer_x.py
+++ dpo_trainer_x.py
@@ -2,26 +2,42 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from unsloth import FastLanguageModel
 import torch
 from trl import DPOTrainer, DPOConfig
+from datasets import load_dataset
 
 model_name = "Qwen/Qwen3-4B"
 
-model = FastLanguageModel.from_pretrained(model_name)
+dataset = load_dataset("Pradheep1647/adrl-dpo")
 
-dataset = "Pradheep1647/AlphaD-RL"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-dpo_condfig = DPOConfig(
-		model_name_or_path = model_name,
-		train_dataset      = dataset,
-		tokenizer         = tokenizer,
-		max_length        = 512,
-		num_train_epochs  = 1,
-		ref_model_sync=True,
+model = FastLanguageModel.from_pretrained(
+  model_name,
+  max_seq_length=4096,
+  load_in_4bit=False,
+  full_finetuning=True,
+  trust_remote_code=True
 )
 
-trainer = DPOTrainer(config = dpo_condfig, model=model, ref_mod=model)
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+
+dpo_config = DPOConfig(
+		do_train=True,
+    per_device_train_batch_size=1,
+    torch_empty_cache_steps=8,
+    num_train_epochs=3,
+    max_steps=500,
+    learning_rate=1e-5,
+    logging_steps=10,
+    sync_ref_model=True,
+    ref_model_sync_steps=32
+)
+
+trainer = DPOTrainer(
+		model=model,
+    ref_model=model,
+    train_dataset=dataset["train"],
+    args=dpo_config,
+		tokenizer=tokenizer,
+)
 
 trainer.train()
 
-trainer.save_pretrained("./adrl-qwen3-4b")
+trainer.save_model("adrl-qwen3-4b")
